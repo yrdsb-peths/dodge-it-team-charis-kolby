7fc9da87795f9cf52f1fa09a6e54bdc06cb2f2d2
kolbyng <440021056@gapps.yrdsb.ca>
2025-11-10 14:28:33 -0500

    added banana monkey to world

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,9 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Banana banana = new Banana();
+        Monkey monkey = new Monkey();
+        addObject(banana, 600, 100);
+        addObject(monkey, 100, 100);
     }
 }
