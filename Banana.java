e2e06af141e5cac7a1881d1146ffcbdcf468091b
Charis <charis.tyc0108@gmail.com>
2025-11-10 14:05:09 -0500

    Added Banana

---
+++ Banana.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+    public void act()
+    {
+        move (-10);
+       
+       if (getX() <= 0){
+           resetBanana();
+       }
+    }
+    
+    public void resetBanana() {
+        int num = Greenfoot.getRandomNumber(2);
+        int x = 600;
+        int y;
+        
+        if (num == 0) {
+            y = 100;
+        } else {
+            y = 300;
+        }
+        
+        setLocation(x, y); // Set location to random position
+        }
+}
