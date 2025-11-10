0dadbbbc5a0afa94364d0d8917b9ac513716a698
kolbyng <440021056@gapps.yrdsb.ca>
2025-11-10 14:01:25 -0500

    created monkey script and image

---
+++ Monkey.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Monkey extends Actor
+{
+    private boolean atTop = true;
+    public void act()
+    {
+        if (Greenfoot.mouseClicked(null))
+        {
+            atTop = !atTop;
+        }
+        
+        if (atTop)
+        {
+            setLocation (100, 100);
+        }
+        else
+        {
+            setLocation(100, 300);
+        }
+    }
+}
